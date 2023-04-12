1419aff2152cac9cc6246cca76e74fb6207a09c3
Refael Abramov <refaelavramov@gmail.com>
2023-04-12 12:29:14 +0300

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/scss/global.scss'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { HomePage } from './views/HomePage';
 import { ProjectDetails } from './views/ProjectDetails';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <HomePage />
